4c48caddff4f1333c450302e8fc390929948ff3a
aleksandarssrajer <aleksandarssrajer@gmail.com>
2024-06-17 22:38:55 +0200

    Cd test 13

--- src/main/java/intens/api/praksa2022/controller/DemoController.java
+++ src/main/java/intens/api/praksa2022/controller/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
     @RequestMapping("/")
     public ResponseEntity<String> getRootDemo() {
 
-        return ResponseEntity.ok().body("Intens Praksa 2050!!");
+        return ResponseEntity.ok().body("Intens Praksa 2060!!");
     }
 }

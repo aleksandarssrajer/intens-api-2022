ee507b01b01245b7f6f607a7dfd48085ae316d85
aleksandarssrajer <aleksandarssrajer@gmail.com>
2024-06-18 01:14:52 +0200

    Final test

--- src/main/java/intens/api/praksa2022/controller/DemoController.java
+++ src/main/java/intens/api/praksa2022/controller/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
     @RequestMapping("/")
     public ResponseEntity<String> getRootDemo() {
 
-        return ResponseEntity.ok().body("Intens Praksa 2021!!");
+        return ResponseEntity.ok().body("Intens Praksa 2025!!");
     }
 }

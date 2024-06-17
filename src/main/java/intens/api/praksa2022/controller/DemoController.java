2ae57b901bc6b52b6b68b518203ea6def21c170d
aleksandarssrajer <aleksandarssrajer@gmail.com>
2024-06-18 00:44:10 +0200

    Test for workflow

--- src/main/java/intens/api/praksa2022/controller/DemoController.java
+++ src/main/java/intens/api/praksa2022/controller/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
     @RequestMapping("/")
     public ResponseEntity<String> getRootDemo() {
         System.out.println("TEST3: CI WORKFLOW");
-        return ResponseEntity.ok().body("Intens Praksa 2022!");
+        return ResponseEntity.ok().body("Intens Praksa 2021!");
     }
 }
